9d8d1c33002969c44ea0097e6d842cd17a82ba68
Nishchit14 <dnishchit@gmail.com>
2018-07-20 11:09:02 +0530

    update code use all event and property

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -1,24 +1,95 @@
 const app = require('express')();
 const http = require('http').Server(app);
-
+const url = require('url');
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server: http });
+//const wss = new WebSocket.Server({server: http});
+const wss1 = new WebSocket.Server({noServer: true});
+const wss2 = new WebSocket.Server({noServer: true});
 
-http.listen(4005, ()=>{
-  console.log('listening on *:4005');
+http.listen(4005, () => {
+    console.log('listening on *:4005');
 });
 
-wss.on('connection',(ws)=> {
+noop = () => {
+}
 
-  ws.on('message', (message)=> {
-    // broadcast to all
-    wss.clients.forEach(c=>{
-      c.send(message);
+heartbeat = () => {
+    console.log('pong called:')
+}
+
+wssconnection = (ws) => {
+    interval
+    ws.isAlive = true;
+    //websocket pong
+    ws.on('pong', heartbeat);
+
+    //console.log(wss.address())
+    //send message
+    ws.on('message', (message) => {
+        // broadcast to all
+        wss.clients.forEach(c => {
+            c.send(message);
+        })
     })
-  })
-});
+}
+/**
+ * ws.ping()
+ */
+ping = () => {
+    wss.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+        ws.isAlive = false;
+        ws.ping(noop);
+    });
+}
+connection = (ws) => {
+    ws.on('message', (message) => {
+        // broadcast to all
+        wss1.clients.forEach(c => {
+            c.send(message);
+        })
+    })
+}
+connection2 = (ws) => {
+    ws.on('message', (message) => {
+        // broadcast to all
+        wss1.clients.forEach(c => {
+            if (c.readyState === ws.open()) {
+                c.send(data);
+            }
+        })
+    })
+}
 
-// Route for group chat (room chat)
-app.get('/group', (req, res)=> {
+/**
+ * multi server connected
+ * @param request
+ * @param socket
+ * @param head
+ */
+upgrade = (request, socket, head) => {
+    const pathname = url.parse(request.url).pathname;
+    if (pathname === '/test') {
+        wss1.handleUpgrade(request, socket, head, function done(ws) {
+            wss1.emit('connection', ws, request);
+        });
+    } else if (pathname === '/bar') {
+        wss2.handleUpgrade(request, socket, head, function done(ws) {
+            wss2.emit('connection', ws, request);
+        });
+    } else {
+        socket.destroy();
+    }
+}
+//const interval = setInterval(ping,30000);
+//wss.on('connection',wssconnection);
+wss1.on('connection', connection);
+wss2.on('connection', connection2);
+http.on('upgrade', upgrade);
+
+app.get('/test', (req, res) => {
+    res.sendFile('test.html', {root: './public'});
+});
+app.get('/group', (req, res) => {
     res.sendFile('group.html', {root: './public'});
 });
